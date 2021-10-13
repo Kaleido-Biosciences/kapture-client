8cca9665c61c6224033cdf1bfb212d997b8b5e1d
Pat Kyle <patrick.kyle@kaleido.com>
2021-10-13 12:08:21 -0400

    Added run information to lims sequencing dto

--- src/main/java/com/kaleido/kaptureclient/domain/dto/LimsSequencingFileDTO.java
+++ src/main/java/com/kaleido/kaptureclient/domain/dto/LimsSequencingFileDTO.java
@@ -9,13 +9,15 @@ public class LimsSequencingFileDTO {
     private String s3Location;
     private String bucket;
     private String datatype;
+    private String runInformation;
 
     public LimsSequencingFileDTO() { }
 
-    public LimsSequencingFileDTO(String s3Location, String bucket, String datatype) {
+    public LimsSequencingFileDTO(String s3Location, String bucket, String datatype, String runInformation) {
         this.s3Location = s3Location;
         this.bucket = bucket;
         this.datatype = datatype;
+        this.runInformation = runInformation;
     }
 
     public String getS3Location() {
@@ -42,11 +44,41 @@ public class LimsSequencingFileDTO {
         this.bucket = bucket;
     }
 
+    public String getRunInformation() {
+        return runInformation;
+    }
+
+    public void setRunInformation(String runInformation) {
+        this.runInformation = runInformation;
+    }
+
+    public LimsSequencingFileDTO S3Location(String s3Location) {
+        this.s3Location = s3Location;
+        return this;
+    }
+
+    public LimsSequencingFileDTO Bucket(String bucket) {
+        this.bucket = bucket;
+        return this;
+    }
+
+    public LimsSequencingFileDTO Datatype(String datatype) {
+        this.datatype = datatype;
+        return this;
+    }
+
+    public LimsSequencingFileDTO RunInformation(String runInformation) {
+        this.runInformation = runInformation;
+        return this;
+    }
+
     @Override
     public String toString() {
         return "LimsSequencingFileDTO{" +
-            "s3Location='" + s3Location + '\'' +
-            ", datatype='" + datatype + '\'' +
-            '}';
+                "s3Location='" + s3Location + '\'' +
+                ", bucket='" + bucket + '\'' +
+                ", datatype='" + datatype + '\'' +
+                ", runInformation='" + runInformation + '\'' +
+                '}';
     }
 }
